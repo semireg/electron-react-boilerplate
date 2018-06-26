5ab1789f661a401ce07856884327e18c14dd23c5
Caylan Larson <caylan@semireg.com>
2018-06-25 21:13:57 -0500

    Broken eslint unable to resolve font-manager

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,4 +1,7 @@
 // @flow
+
+import * as FontManager from 'font-manager';
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
@@ -9,12 +12,17 @@ export default class Home extends Component<Props> {
   props: Props;
 
   render() {
+    const fonts = JSON.stringify(FontManager.getAvailableFontsSync());
+
     return (
       <div>
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
           <Link to="/counter">to Counter</Link>
         </div>
+        <span>
+          {fonts}
+        </span>
       </div>
     );
   }
